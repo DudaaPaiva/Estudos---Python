91a4ec3ccf9f6e0bfe16f894c57234f3920ebe5b
Eduarda Paiva <eduarda.a.paiva@gmail.com>
2025-03-14 18:03:58 -0300

    ano bissexto

---
+++ desafio32.py
@@ -0,0 +1,9 @@
+ #Programa que determina se um ano é bissexto 
+from datetime import date
+ano = int(input(f'Qual ano quer analisar? Digite 0 para analisar o ano atual: '))
+if ano == 0:
+  ano = date.today().year #pega ano atual
+if ano%4 == 0 and ano%100 !=0 or ano%400 ==0:
+  print(f'O ano {ano} é bissexto!')
+else:
+  print(f'O ano {ano} não é bissexto.')
